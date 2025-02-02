bf8f6fde2b7ad3a7664796bd3cf828206e686f3d
Ahmad Raza <mustafaraza03898@gmail.com>
2025-02-02 18:52:26 +0530

    update in main.java

--- src/com/ahmad/app/Main.java
+++ src/com/ahmad/app/Main.java
@@ -1,20 +1,30 @@
 package com.ahmad.app;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.ahmad.beans.LapTop;
+import com.ahmad.beans.Programmer;
 import com.ahmad.beans.Student;
 
 public class Main {
 	public static void main(String[] args) {
-		//creating resource based on file
-		Resource rs=new ClassPathResource("/com/ahmad/resources/config(bF).xml");
-		BeanFactory bean=new XmlBeanFactory(rs);
-		Student s=(Student)bean.getBean("stu1");
+	
+		ApplicationContext container=new ClassPathXmlApplicationContext("/com/ahmad/resources/config(bF).xml");
+		 //Student s1=(Student)container.getBean("stu1");
+		 //Student s2=(Student)container.getBean("stu2");
+		 //Student s2=(Student)container.getBean("stu3");
+
+
+		//System.out.println("name: "+s1.getName()+"\nrollno: "+s1.getRollno());
+		//System.out.println("name: "+s2.getName()+"\nrollno: "+s2.getRollno());
+  
+		LapTop lt=(LapTop)container.getBean("l1");
+		Programmer pr=(Programmer)container.getBean("p1");
 		
-		System.out.println("name: "+s.getName()+"\nrollno: "+s.getRollno());
+		System.out.println("name: "+lt.getbrand());
+		System.out.println("name: "+pr.getName());
+		System.out.println("name: "+pr.getLt().getbrand());
 	}
 
 }
