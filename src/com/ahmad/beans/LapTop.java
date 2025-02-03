ee7e56727de6e2dc58016b1f06f98202a200b244
Ahmad Raza <mustafaraza03898@gmail.com>
2025-02-03 18:53:57 +0530

    update in Dependent class

--- src/com/ahmad/beans/LapTop.java
+++ src/com/ahmad/beans/LapTop.java
@@ -1,19 +1,26 @@
 package com.ahmad.beans;
 
-public class LapTop {
-	private String brand;
-
+public class LapTop implements Device{
+	private String name;
 	public LapTop() {
        System.out.println("LapTop created.....");
 	}
-
-	public String getbrand() {
-		return brand;
+	
+	public String getName() {
+		return name;
 	}
 
-	public void setbrand(String name) {
-		this.brand = name;
-		System.out.println("Laptop intialized by name setters..");
+	public void setName(String name) {
+		this.name = name;
+		System.out.println("LapTop name intialized/injected..");
+		
+	}
+
+	@Override
+	public void start() {
+      System.out.println("Laptop started...");	
+      System.out.println("LapTop Name:"+name);
+
 	}
 	
 
